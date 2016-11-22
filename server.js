0eb13133c26099fad95484043d286d59780dc508
venkateshsrm <venkateshsrm@gmail.com>
2016-11-21 19:29:56 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,22 +8,6 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'home.html'));
-});
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'about.html'));
-});
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'news.html'));
-});
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'Portofolio.html'));
-});
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'contact.html'));
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
