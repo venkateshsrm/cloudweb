111a182e736900109f09a95f84a07755402fc136
venkateshsrm <venkateshsrm@gmail.com>
2016-11-21 19:32:53 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/dynamic.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'dynamic.jpg'));
 });
 
