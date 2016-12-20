f66ba5eb789dbbfaa5e3215dc5946430b847387a
venkateshsrm <venkateshsrm@gmail.com>
2016-12-20 15:27:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/bootstrap.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'bootstrap.css'));
 });
+app.get('/ui/bootstrap.min.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'bootstrap.min.js'));
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
